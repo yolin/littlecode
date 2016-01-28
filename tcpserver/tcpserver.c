5ee10f9ffaa6bc380f6dd02c94beb93c947e945b
yolin <yolin.tsai@senao.com>
2016-01-28 13:53:03 +0800

    tcpserver with pthread

--- tcpserver/tcpserver.c
+++ tcpserver/tcpserver.c
@@ -8,9 +8,13 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h> 
+#include <fcntl.h>
+#include <signal.h>
+#include <pthread.h> 
 
-int main(int argc, char *argv[])
-{
+char command[50];
+
+void *do_web_server(void *argu) {
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr; 
 
@@ -38,15 +42,20 @@ int main(int argc, char *argv[])
     printf("socket snd buffer size [%d]\n", sndbuf);
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
-
     listen(listenfd, 10); 
 
+    //signal(SIGCHLD, SIG_IGN);
 
     FILE *fp;
 
-    while(1)
+    while (1)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+        printf("pthread do_webserver:\n");
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);    
+
+        printf("copy command!!!\n");
+        snprintf(command, sizeof(command), "/littlecode/tcpserver/1.sh");
+        printf("copy command[%s]!!!\n",command);
 
         //ticks = time(NULL);
         //snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
@@ -94,5 +103,63 @@ int main(int argc, char *argv[])
 
         close(connfd);
         sleep(1);
-     }
+
+    }    
+    return NULL;    
+}    
+
+void *do_command(void *argu) {     // 每隔一秒鐘印出一次 Mary 的函數
+    FILE *fp;
+    int rc =0 ;
+    char result_buf[50];
+
+    while (1) {    
+
+        printf("pthread do_command:\n");
+        fp = popen(command, "r");
+
+        if(NULL == fp)
+        {
+            perror("popen error!");
+            exit(1);
+        }
+        while(fgets(result_buf, sizeof(result_buf), fp) != NULL)
+        {
+            /*为了下面输出好看些，把命令返回的换行符去掉*/
+            if('\n' == result_buf[strlen(result_buf)-1])
+            {
+                result_buf[strlen(result_buf)-1] = '\0';
+            }
+            printf("command[%s] output[%s]\n", command, result_buf);
+        }
+
+        /*等待命令执行完毕并关闭管道及文件指针*/
+        rc = pclose(fp);
+        if(-1 == rc)
+        {
+            perror("pclose error!");
+            exit(1);
+        }
+        else
+        {
+            printf("command[%s] status[%d] return[%d]\n", command, rc, WEXITSTATUS(rc));
+        }
+        sleep(1);
+    } 
+    return NULL;    
+}    
+
+int main(int argc, char *argv[])
+{
+
+    pthread_t thread1, thread2;     // 宣告兩個執行緒
+    pthread_create(&thread1, NULL, &do_web_server, NULL);    // 執行緒 print_george
+    pthread_create(&thread2, NULL, &do_command, NULL);    // 執行緒 print_mary
+    while (1) {     // 主程式每隔一秒鐘
+        printf("----------------\n");    // 就印出分隔行
+        sleep(1);     // 停止一秒鐘
+    }    
+
+
+
 }
