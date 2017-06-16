c863cc4cf167da70764568b4af062d15797e891b
yolin <yolin.tsai@senao.com>
2017-06-16 10:45:02 +0800

    python test

--- python/gui.py
+++ python/gui.py
@@ -3,9 +3,48 @@
 import tkinter as tk
 import os
 
-root = tk.Tk()
-tfield = tk.Text(root)
-tfield.pack()
-for line in os.popen("ls", 'r'):
-    tfield.insert("end", line)
-root.mainloop()
+
+def center(toplevel):
+    toplevel.update_idletasks()
+    w = toplevel.winfo_screenwidth()
+    h = toplevel.winfo_screenheight()
+    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+    x = w/2 - size[0]/2
+    y = h/2 - size[1]/2
+    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
+
+def clickOK():
+    global count
+    count = count + 1
+    #label2.configure(text = "Click OK " + str(count) + " times")
+
+def clickReload():
+    for line in os.popen("ls", 'r'):
+        tfield.insert("end", line)
+
+
+
+count = 0
+win = tk.Tk()
+win.title("Atlantis info")
+
+#label1 = tk.Label(win, text = "http://atlantis.senao.com")
+#label2 = tk.Label(win, text = "Result")
+
+button1 = tk.Button(win, text = "OK", command = clickOK)
+button2 = tk.Button(win, text = "Reload", command = clickReload)
+
+tfield = tk.Text(win)
+scroll = tk.Scrollbar(win, orient = "vertical", command = tfield.yview)
+
+
+#label1.grid(column = 0, row = 0)
+#label2.grid(column = 0, row = 1)
+button1.grid(column = 0, row = 0)
+button2.grid(column = 0, row = 1)
+tfield.grid(column = 1, row = 0, rowspan = 2)
+#scroll.grid(column = 2, row = 0, rowspan = 15, columnspan = 1, sticky = "ns")
+scroll.grid(column = 2, row = 0, rowspan = 2, sticky = "ns")
+
+center(win)
+win.mainloop()
