859fdb9440dd0bf28b99b56e7b08f1987c9ffa13
yolin <yolin.tsai@senao.com>
2019-05-15 14:05:30 +0800

    test

--- getip/1.c
+++ getip/1.c
@@ -13,6 +13,7 @@ int main()
 {
     int fd;
     struct ifreq ifr;
+    char mac[6]={0};
 
     fd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -20,14 +21,22 @@ int main()
     ifr.ifr_addr.sa_family = AF_INET;
 
     /* I want IP address attached to "eth0" */
-    strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+    strncpy(ifr.ifr_name, "enp2s0", IFNAMSIZ-1);
 
     ioctl(fd, SIOCGIFADDR, &ifr);
+
+    /* I want mac attached to "eth0" */
+    if (ioctl(fd, SIOCGIFHWADDR, (caddr_t)&ifr) < 0) {
+        return 1;
+    }
+    memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+
 
     close(fd);
 
     /* display result */
     printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+    printf("%.2x:%.2x:%.2x:%.2x:%.2x:%.2x", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
 
     return 0;
 }
