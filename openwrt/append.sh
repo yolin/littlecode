ad9cf791925d2c3fe25ef4e98ae176e1713d937f
yolin <yolin.tsai@senao.com>
2021-09-10 12:42:16 +0800

    mod

--- openwrt/append.sh
+++ openwrt/append.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-append() {                                                                                                                                                                                    local var="$1"                                                                                                                                                                        local value="$2"                                                                                                                                                                      local sep="${3:- }"                                                                                                                                                                   
+append() {
+    local var="$1" local value="$2" local sep="${3:- }"
     eval "export ${NO_EXPORT:+-n} -- \"$var=\${$var:+\${$var}\${value:+\$sep}}\$value\""                                                                                                  
 }
 
 
 append "aaa" "bbb"
-append "aaa" "ccc" "xxx"
-append "aaa" "ddd" "lll"
-append "aaa" "eee"
+echo $aaa
+append "aaa" "ccc" ","
+echo $aaa
+append "aaa" "ddd" ","
+echo $aaa
+append "aaa" "eee" ","
 
 echo $aaa
